324343a5376ae5db2805e668ef82e15291ba9ec1
Corbin Farley <cbnman99@gmail.com>
2020-10-15 12:41:00 -0500

    Fixed Menu, smoothe scroll, and spacers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//const $=require('jquery');
+
 function menuMobile(){
   var x = document.getElementById("myMenu");
   if (x.style.display === "block") {
@@ -6,3 +8,15 @@ function menuMobile(){
     x.style.display = "block";
   }
 }
+
+//Smooth scrolling
+$(".linker").on('click', function() {
+  const hash = this.hash;
+
+  const linkto = $(hash).position().top;
+
+  $('html, body').animate({
+    scrollTop: linkto
+  }, 900
+);
+});
