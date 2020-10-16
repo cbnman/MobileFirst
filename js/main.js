c72eea49eef9e9b2ce036ac975d252bb83db374a
Corbin Farley <cbnman99@gmail.com>
2020-10-16 11:32:48 -0500

    Adding Media Query

--- js/main.js
+++ js/main.js
@@ -40,10 +40,10 @@ for (i = 0; i < services.length; i++) {
 //
 // function scrollFunction() {
 //   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("topnav").style.padding = "30px 10px";
+//     document.getElementById("navbar").style.padding = "30px 10px";
 //     //document.getElementById("logo").style.fontSize = "25px";
 //   } else {
-//     document.getElementById("topnav").style.padding = "80px 10px";
+//     document.getElementById("navbar").style.padding = "80px 10px";
 //     //document.getElementById("logo").style.fontSize = "35px";
 //   }
 // }
