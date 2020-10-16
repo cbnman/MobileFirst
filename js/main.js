14795e14cb33aae902fe6c63d93660fcccd2783c
Corbin Farley <cbnman99@gmail.com>
2020-10-15 22:02:04 -0500

    Added Services table and favicon

--- js/main.js
+++ js/main.js
@@ -20,3 +20,31 @@ $(".linker").on('click', function() {
   }, 900
 );
 });
+
+var services = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < services.length; i++) {
+  services[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// //Menu Shrink with scroll
+// window.onscroll = function() {scrollFunction()};
+//
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("topnav").style.padding = "30px 10px";
+//     //document.getElementById("logo").style.fontSize = "25px";
+//   } else {
+//     document.getElementById("topnav").style.padding = "80px 10px";
+//     //document.getElementById("logo").style.fontSize = "35px";
+//   }
+// }
